52008485401cb91fb7881e1cfe8775a779b4c67e
ozaki <fs65ugumori@yahoo.co.jp>
2017-10-18 23:53:40 +0900

    実際に撮影するようにした

--- src/containers/CameraResultScreen.js
+++ src/containers/CameraResultScreen.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Dimensions, TouchableOpacity, Image, Text } from 'react-native';
 
 class CameraResultScreen extends Component {
   static navigationOptions = {
     title: '撮影結果',
   }
-  render() {
-    return (
-      <View />
 
-    );
+  onPress = () => this.props.navigation.goBack();
+
+  render() {
+    const { height, width } = Dimensions.get('window')
+    return (
+      <TouchableOpacity onPress={this.onPress} style={{ flex: 1, backgroundColor: 'lightgreen' }}>
+        <Image
+           resizeMode="contain"
+           source={{ uri: this.props.navigation.state.params.path }}
+           style={{ height: height / 2, width: width / 2, alignSelf: 'center' }}/>
+        <Text>{JSON.stringify(this.props.navigation.state)}</Text>
+      </TouchableOpacity>
+    )
   }
 }
 
--- src/containers/CameraScreen.js
+++ src/containers/CameraScreen.js
@@ -4,17 +4,19 @@ import Camera from 'react-native-camera';
 
 class CameraScreen extends Component {
   static navigationOptions = {
-    header: null,
+    title: '撮影',
   }
 
-  onPress = () => {
-    this.props.navigation.navigate('CameraResultScreen');
+  onPress = async () => {
+    const data = await this.camera.capture();
+    this.props.navigation.navigate('CameraResultScreen', { path: data.path });
   }
 
   render() {
     return (
       <TouchableOpacity style={{ flex: 1 }} onPress={this.onPress}>
         <Camera
+           ref={camera => this.camera = camera}
            style={{ flex: 1 }}
            captureTarget={Camera.constants.CaptureTarget.temp}
            flashMode={Camera.constants.FlashMode.off} />
